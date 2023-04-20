c2267b3e5b8eaac10245f57aa7c2e8737db1ea3c
IlyaV45 <papacarlo.dnepr@gmail.com>
2023-04-20 11:14:11 +0300

    Add let $ const

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-// alert(123)
 
-console.log(123)
+let userName = 'Brad';
+let userAge;
+userAge = 69;
+
+const userDataOfBirth = "11_01_1991";
+
+// console.log(userName);
+// console.log(userAge);
+
+userAge = 60;
+userName = "Bradly";
+
+const genderUser = "male";
+
+const AMOUNT_DAYS_WEEK = 7;
+
+
+
+
+let number = 12;
+
+{
+let number = 45;
+console.log(number);
+}
+console.log(number);
