6e3079f47430c67d295e0f98f88cda2ea968a6f2
IlyaV45 <papacarlo.dnepr@gmail.com>
2023-04-26 12:31:12 +0300

    ADD && || ??

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
 
-// let number1 = '12';
-// let number2 = '102';
+const condition1 = 2 && true && 'qwe' && Nan;
+console.log(condition1);
 
-// console.log(number1 < number2);
-// console.log(number1 <= number2);
-// console.log(number1 > number2);
-// console.log(number1 >= number2);
+const condition2 = 2 || true || 'qwe' || Nan;
+console.log(condition2);
 
-// const str1 = 'cat';
-// const str2 = 'dog';
-// console.log(str1<str2);
+const condition3 = false ?? "default";
+console.log(condition3);
 
-let number1 = 50;
-const str1 = '50';
-
-console.log(number1 < str1);
-console.log(number1 > str1);
-
-console.log(true >= false); //1>0
 
 
 
