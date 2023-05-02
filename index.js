048f14bec49510f75f1907ca03beaab8e00e0a7f
IlyaV45 <papacarlo.dnepr@gmail.com>
2023-05-02 14:37:30 +0300

    Add function

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-const userInput = prompt("Enter number: ", 5)
+function logSumTwoUserInputNumbers() {
+  const userInput1 = prompt("Enter number: ");
+  const userInput2 = prompt("Enter number: ");
 
+  const checkUserInput1 =
+    userInput1 === "" ||
+    userInput1 === null ||
+    Number.isNaN(Number(userInput1));
+  const checkUserInput2 =
+    userInput2 === "" ||
+    userInput2 === null ||
+    Number.isNaN(Number(userInput2));
 
-if (userInput === "" || userInput === null || Number.isNaN(Number(userInput))) {
-  console.log("error");
-} else {
-  console.log(userInput, Number(userInput));
+  if (checkUserInput1 || checkUserInput2) {
+    console.log("error! value must be number!");
+  } else {
+    const suma = Number(userInput1) + Number(userInput2);
+    console.log(suma);
+  }
 }
+logSumTwoUserInputNumbers()
